1ed0ec0fc69e53fb062b9d0ed0fbfd1603a1a17d
Pereira Ricardo <hello@nunopress.com>
2016-09-06 18:19:58 +0200

    Fixed error for the snippet without a image cover

--- site/snippets/home/tiles.php
+++ site/snippets/home/tiles.php
@@ -1,9 +1,11 @@
 <section class="tiles">
     <?php foreach ($page->children()->visible() as $p): ?>
         <article class="<?php e($p->style()->isNotEmpty(), "style" . $p->style(), "style1") ?>">
-                                            <span class="image">
-                                                <img src="<?php echo $p->image($p->cover())->url() ?>" alt="" />
-                                            </span>
+            <?php if ($p_image = $p->image($p->cover())): ?>
+                <span class="image">
+                    <img src="<?php echo $p_image->url() ?>" alt="" />
+                </span>
+            <?php endif; ?>
             <a href="<?php echo $p->url() ?>">
                 <h2><?php echo $p->title()->html() ?></h2>
 
